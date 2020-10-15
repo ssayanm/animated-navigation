07023fcbb6cec036e612ab25fd6fa4f4f9eda67f
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-10-15 09:33:33 +0530

    toggling menu is working

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
 
-const toggleNav = () => {
-    //Toggle: menu bars open/closed
-    return menuBars.classList.toggle('change')
 
-  } 
 
-// function toggleNav(){
-//     menuBars.classList.toggle('change')
-// }
+function toggleNav() {
+    // Toggle: menu bars open/closed
+    menuBars.classList.toggle("change");
+    //Toggle: menu active
+    overlay.classList.toggle("overlay-active");
+    if (overlay.classList.contains("overlay-active")) {
+      // animate in overlay
+      overlay.classList.remove("overlay-slide-left");
+      overlay.classList.add("overlay-slide-right");
+    } else {
+      // animate ut overlay
+      overlay.classList.remove("overlay-slide-right");
+      overlay.classList.add("overlay-slide-left");
+    }
+}
 
 // Event listeners
 menuBars.addEventListener('click', toggleNav);
 nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav)
-nav3.addEventListener('click', toggleNav)
-nav4.addEventListener('click', toggleNav)
-nav5.addEventListener('click', toggleNav)
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
