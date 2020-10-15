8cd8bd4def9ecdaa5a2c981693f5534ecd68305f
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-10-15 10:30:19 +0530

    toggling menu is working and updated css

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5]
 
-
+//control navigation animation
+function navAnimation(direction1, direction2){
+    navItems.forEach((nav, i)=> {
+        nav.classList.replace(`slide-${direction1}-${i+1}`, `slide-${direction2}-${i+1}`)
+    })
+}
 
 function toggleNav() {
     // Toggle: menu bars open/closed
@@ -15,19 +21,20 @@ function toggleNav() {
     overlay.classList.toggle("overlay-active");
     if (overlay.classList.contains("overlay-active")) {
       // animate in overlay
-      overlay.classList.remove("overlay-slide-left");
-      overlay.classList.add("overlay-slide-right");
+      overlay.classList.replace("overlay-slide-left", "overlay-slide-right")
+      // animate in - nav items
+     navAnimation('out', 'in');
+
     } else {
-      // animate ut overlay
-      overlay.classList.remove("overlay-slide-right");
-      overlay.classList.add("overlay-slide-left");
+      // animate out overlay
+      overlay.classList.replace("overlay-slide-right", "overlay-slide-left")
+       // animate out - nav items
+      navAnimation('in', 'out');
     }
 }
 
 // Event listeners
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
-nav3.addEventListener('click', toggleNav);
-nav4.addEventListener('click', toggleNav);
-nav5.addEventListener('click', toggleNav);
+navItems.forEach((nav)=> {
+    nav.addEventListener("click", toggleNav)
+})
